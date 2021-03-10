03f1a693226aed013041260a34dd442ca3235a29
liushiming <487005831@qq.com>
2021-03-10 15:40:46 +0800

    makefile

--- Makefile
+++
@@ -1,6 +0,0 @@
-SHELL = /bin/sh
-
-default_target: run
-
-run:
-	echo welcome go to code4cpp
--- ch17/main.cpp
+++ ch17/main.cpp
@@ -1,5 +1,9 @@
 #include "main.h"
 
+const int *p = new int(100);
+
+constexpr int port = 100;
+
 int main()
 {
     cout << "hello" << endl;
